8289c5053067af602ceb1dacd732efa42b0b6829
yutaono <vrf.ced@gmail.com>
2013-06-01 05:02:57 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,99 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    mongoose = require('mongoose');
+
+var app = module.exports = express.createServer();
+// var app = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  // console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+
+// mongoose
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  message: String,
+  px: Number,
+  py: Number,
+  color_num: Number,
+  visible: Boolean,
+  created: Date
+});
+mongoose.model('User', UserSchema);
+var uri = process.env.MONGOHQ_URL || 'mongodb://localhost/59bb'
+// mongoose.connect('mongodb://133.242.151.118/59bb');
+// mongoose.connect(uri);
+var User = mongoose.model('User');
+
+// Socket
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket){
+
+  socket.on('msg update', function(){
+    User.find({"visible":true}, function(err, docs){
+      socket.emit('msg open', docs);
+    });
+  });
+
+  console.log('conencted');
+
+  socket.on('msg send', function(msg, px, py, color_num, created){
+    socket.emit('msg push', msg, px, py, color_num, created);
+    socket.broadcast.emit('msg push', msg, px, py, color_num, created);
+    var user = new User();
+    user.message = msg;
+    user.px = px;
+    user.py = py;
+    user.color_num = color_num;
+    user.visible = true;
+    user.created = new Date();
+    user.save(function(err) {
+      if(err) { console.log(err); }
+    });
+  });
+
+  socket.on('msg visible_false', function(px, py){
+    User.update({"px":px, "py":py}, { $set:{"visible":false} }, false, true);
+  });
+
+  socket.on('deleteDB', function(){
+    socket.emit('db drop');
+    socket.broadcast.emit('db drop');
+    User.find().remove();
+  });
+
+  socket.on('disconnect', function(){
+    console.log('disconnected');
+  });
+});
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,80 @@
+$(function(){
+
+	// var socket = io.connect('http://133.242.151.118:3000');
+	// var socket = io.connect('http://www.snake4.mobi/fnbb/')
+	var socket = io.connect('http://fnbb.herokuapp.com/')
+	// var socket = io.connect('http://localhost')
+	// var socket = new io.Socket();
+	// socket.connect();
+	socket.on('connect', function(){
+		socket.emit('msg update');
+	});
+
+	var pos_x, pos_y;
+	var textarea_click_flag = false;
+	var pos_array = new Array();
+	var color = new Array('red', 'darkblue', 'green', 'orange', 'purple', 'gray', 'black', 'darkblue', 'maroon', 'fuchsia');
+	var max_fadeout_time = 59000; // 59sec
+
+	$('#zone').click( function(e){
+		var id = String(e.pageX) + '_' +String(e.pageY);
+		var color_num = Math.floor(Math.random()*color.length);
+
+		$(this).append("<input class='massage' id="+id+" value='' />");
+		$('input#'+id).css("left",e.pageX).css("top",e.pageY).css("color", color[color_num]);
+
+		$('input#'+id).focus().blur(function(){
+			if($(this).val()!=''){
+				socket.emit('msg send', $(this).val(), e.pageX, e.pageY, color_num);
+			}
+			$(this).remove();
+		});
+
+		$('input#'+id).keypress(function(e){
+			var c = e.which ? e.which : e.keyCode;
+			if(c==13){
+				$(this).blur();
+			}
+		});
+
+	});
+
+	socket.on('msg push', function(msg, px, py, color_num, created){
+		var id = String(px) + '_' +String(py);
+
+		$('#zone').append("<span class='text' id='text_"+id+"''>"+msg+"</span>");
+		$('span#text_'+id).css("left", px).css("top", py).css("color", color[color_num]);
+		$('span#text_'+id).fadeOut(max_fadeout_time, function(){
+			socket.emit('msg visible_false', px, py);
+		});
+		// }
+	});
+
+	socket.on('msg open', function(docs){
+		if(docs.length == 0){
+			return;
+		} else {
+			$.each(docs, function(key, value){
+				var id = String(value.px) + '_' +String(value.py);
+				var fadeout_time = max_fadeout_time - (new Date() - new Date(value.created));
+
+				$('#zone').append("<span class='text' id='text_"+id+"''>"+value.message+"</span>");
+				$('span#text_'+id).css("left", value.px).css("top", value.py).css("color", color[value.color_num]);
+				$('span#text_'+id).css("opacity", fadeout_time/max_fadeout_time);
+
+				if(fadeout_time < max_fadeout_time ){
+					$('span#text_'+id).fadeOut(fadeout_time, function(){
+						socket.emit('msg visible_false', value.px, value.py);
+					});
+				} else {
+					socket.emit('msg visible_false', value.px, value.py);
+				}
+			});
+		}
+	});
+
+	socket.on('msg update', function(msg){
+		console.log(msg);
+	});
+
+});
