1431cf5bc90fc4ae5f8d713dce7f94c01d6384bc
yutaono <vrf.ced@gmail.com>
2013-06-02 17:08:27 +0900

    add posts.ejs

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: '59sec white board' })
 };
+
+exports.posts = function(req, res){
+  res.render('posts', { title: '59sec white board 2' })
+};
