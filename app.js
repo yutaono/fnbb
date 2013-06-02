3621d2fc2a47d4d501e0382fa9075c0ce77bc351
yutaono <vrf.ced@gmail.com>
2013-06-02 17:53:43 +0900

    fix bugs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express'),
     routes = require('./routes'),
-    posts_handler = require('./routes/posts'),
+    // posts_handler = require('./routes/posts'),
     mongoose = require('mongoose');
 
 var app = module.exports = express.createServer();
@@ -32,8 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.get('/posts', posts_handler.index);
-
+// app.get('/posts', posts_handler.index);
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
